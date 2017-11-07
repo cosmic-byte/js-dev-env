ec560b2abdefabe6621685f296411fde46e5efc0
gregobinna <gregobinna@gmail.com>
2017-11-07 11:05:50 +0100

    automate build process

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -7,11 +7,13 @@ var port = 3000;
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'../src/index.html'));
 });
-
+app.get('/hello',function(req, res){
+		res.send('Hello fam');
+});
 app.listen(port, function(err){
     if(err){
         console.log(err);
     }else{
         open('http://localhost:'+port);
     }
-})
+});
