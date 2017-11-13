1d6fdd5e842e51da246e116b293c9fbfdcc4ec07
gregobinna <gregobinna@gmail.com>
2017-11-13 12:07:50 +0100

    add jsdom for testing

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,6 +8,7 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+/* eslint-disable no-console */
 app.use(require('webpack-dev-middleware')(compiler,{
 		noInfo: true,
 		publicPath: config.output.publicPath
