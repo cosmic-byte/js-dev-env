40fc75bf784fba177b0cd97a31dcc7448308c029
gregobinna <gregobinna@gmail.com>
2017-11-23 11:07:37 +0100

    deploy to huroku and surge

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,6 @@
 
 export default function getBaseUrl(){
-	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://fierce-forest-10468.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url){
